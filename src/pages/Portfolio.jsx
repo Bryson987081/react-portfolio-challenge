5332c0113c7a334cf7e185dc41a45ca7829153a0
Bryson987081 <166461780+Bryson987081@users.noreply.github.com>
2024-09-23 12:50:29 -0400

    added temporary cards into Portfolio.jsx

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -2,9 +2,53 @@ export default function Portfolio() {
     return (
         <div>
             <h1>My Work</h1>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus maxime cum amet placeat, ratione quia error nihil porro, dignissimos nostrum consequatur voluptatibus perspiciatis unde sint, hic veniam reprehenderit est eos. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, labore dignissimos quae asperiores numquam culpa enim illum harum, laudantium veniam, nesciunt ipsum ratione deserunt accusantium. Cumque totam corporis aut harum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore praesentium soluta rerum sequi recusandae voluptatibus ad, numquam magnam quos dolorem dolores laborum aliquam dignissimos culpa quae natus odit voluptatum fugiat?
-            </p>
-        </div>
+            <div class="card" style="width: 18rem;">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+                <div class="card" style="width: 18rem;">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+                    <div class="card" style="width: 18rem;">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+                        <div class="card" style="width: 18rem;">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+                            <div class="card" style="width: 18rem;">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+                                <div class="card" style="width: 18rem;">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
     );
 }
