efcbcb4dbed21220005256d7f48f9e51fbb1d989
Bryson <brysob7617@gmail.com>
2024-09-23 21:50:42 -0400

    added style updates to about me page

--- src/componets/Header.jsx
+++ src/componets/Header.jsx
@@ -1,7 +1,7 @@
 function Header() {
     return (
         <div style={{backgroundColor:'#8EACCD'}}>
-        <header className="header sticky-top py-5 mx-3">
+        <header className="header sticky-top py-5 ml-3">
             <h1 className="text-center">Bryson Shoffner's Portfolio</h1>
         </header>
         </div>
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,10 +1,24 @@
 export default function About() {
     return (
-        <div className="card">
-            <h1 className="card-header text-center p-4">About Me</h1>
-            <p className="card-body">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus maxime cum amet placeat, ratione quia error nihil porro, dignissimos nostrum consequatur voluptatibus perspiciatis unde sint, hic veniam reprehenderit est eos. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, labore dignissimos quae asperiores numquam culpa enim illum harum, laudantium veniam, nesciunt ipsum ratione deserunt accusantium. Cumque totam corporis aut harum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore praesentium soluta rerum sequi recusandae voluptatibus ad, numquam magnam quos dolorem dolores laborum aliquam dignissimos culpa quae natus odit voluptatum fugiat?
-            </p>
+        <div className="card mx-auto my-4" style={{ maxWidth: '600px' }}>
+            <h1 className="card-header text-center">About Me</h1>
+            <img 
+                src="https://placehold.co/400x400" 
+                alt="Placeholder" 
+                className="card-img-top"
+            />
+            <div className="card-body">
+                <p className="card-text">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus maxime cum amet placeat, 
+                    ratione quia error nihil porro, dignissimos nostrum consequatur voluptatibus perspiciatis unde 
+                    sint, hic veniam reprehenderit est eos. Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                    Sunt, labore dignissimos quae asperiores numquam culpa enim illum harum, laudantium veniam, 
+                    nesciunt ipsum ratione deserunt accusantium. Cumque totam corporis aut harum. Lorem ipsum dolor 
+                    sit amet consectetur adipisicing elit. Tempore praesentium soluta rerum sequi recusandae 
+                    voluptatibus ad, numquam magnam quos dolorem dolores laborum aliquam dignissimos culpa quae 
+                    natus odit voluptatum fugiat?
+                </p>
+            </div>
         </div>
     );
 }
